dfe74c7d3873d43cb7ecdd207a3f781d17a9de7c
modgil <aditimodgil3@gmail.com>
2016-08-18 21:54:43 +0530

    atm
    java project

---
+++ ATM/AtmSimulator.java
@@ -0,0 +1,11 @@
+
+public class AtmSimulator {
+      public static void main(String[] args) {
+        // Create application frame.
+        AtmSimulatorFrame frame = new AtmSimulatorFrame();
+        
+        // Show frame
+        frame.setVisible(true);
+    }
+
+}
---
+++ ATM/Mainpanel.java
@@ -0,0 +1,13 @@
+
+public class Mainpanel {
+    
+	 public static void main(String[] args)
+        {
+               
+               Atm a= new Atm();
+               
+               
+        }
+        
+
+}
